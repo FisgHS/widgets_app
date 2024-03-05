6b2b5b7128fe6e9eb1515d784eaffbe4e73c34d7
Francisco Silva <silvagutierrez010@gmail.com>
2024-03-05 16:38:52 -0300

    Create theme and styles

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.blueGrey,
+  Colors.brown,
+  Colors.deepOrange,
+  Colors.indigo
+];
+
+class AppTheme {
+
+  final int selectecColor;
+
+  AppTheme({
+    this.selectecColor = 0
+  }):assert( selectecColor >= 0, 'Selected color must be greater than 0' ) ,
+      assert( selectecColor <= colorList.length, 'Selected color must be less or equal than ${colorList.length-1}' ) ;
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectecColor ]
+  );
+
+}
