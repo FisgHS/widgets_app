a8ff9a180acfe7754d9bf5ab0281119b1c8a5536
Francisco Silva <silvagutierrez010@gmail.com>
2024-03-07 18:24:02 -0300

    Create custom button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -79,9 +79,35 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
 
-            // Todo: Custom Button
+            const CustomButton(),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Custom', style: TextStyle(color: Colors.white),)
+          ),
         ),
       ),
     );
